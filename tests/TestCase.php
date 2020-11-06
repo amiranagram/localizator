cec3a68fdbc5b38ccd475b7178792da1c5085ad5
Amir Rami <amirrami.ce@gmail.com>
2020-11-06 21:41:40 +0100

    Update tests

---
+++ tests/TestCase.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Amirami\Localizator\Tests;
+
+use Amirami\Localizator\ServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+class TestCase extends Orchestra
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    protected function getPackageProviders($app): array
+    {
+        return [
+            ServiceProvider::class,
+        ];
+    }
+
+    public function getEnvironmentSetUp($app): void
+    {
+
+    }
+}
