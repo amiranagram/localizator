bbbe6b902e5241493707c01f5f6c17e4112792bf
Amir <a.rami@adaptech.me>
2020-11-09 12:14:30 +0100

    Create mock resources structure and write some helper methods

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,25 @@ namespace Amirami\Localizator\Tests;
 
 use Amirami\Localizator\ServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
+use Symfony\Component\Finder\Finder;
 
 class TestCase extends Orchestra
 {
+    /**
+     * @var string[]
+     */
+    protected $searchDirectories = ['resources/views'];
+
+    /**
+     * @var string[]
+     */
+    protected $searchPatterns = ['*.php'];
+
+    /**
+     * @var string[]
+     */
+    protected $searchFunctions = ['__', 'trans', '@lang'];
+
     /**
      * Setup the test environment.
      *
@@ -39,12 +55,36 @@ class TestCase extends Orchestra
      */
     public function getEnvironmentSetUp($app)
     {
-        $app->setBasePath(__DIR__ . DIRECTORY_SEPARATOR . 'mock');
+        $app->setBasePath(__DIR__ . DIRECTORY_SEPARATOR . 'Mock');
 
         $app['config']->set('app.locale', 'en');
 
-        $app['config']->set('localizator.search.dirs', ['resources']);
-        $app['config']->set('localizator.search.patterns', ['*.blade.php']);
-        $app['config']->set('localizator.search.functions', ['__', 'trans', '@lang']);
+        $app['config']->set('localizator.search.dirs', $this->searchDirectories);
+        $app['config']->set('localizator.search.patterns', $this->searchPatterns);
+        $app['config']->set('localizator.search.functions', $this->searchFunctions);
+    }
+
+    /**
+     *
+     */
+    protected function cleanLangDirectory(): void
+    {
+        $files = (new Finder())->in(resource_path('lang'))->files();
+
+        foreach ($files as $file) {
+            unlink($file->getPathname());
+        }
+    }
+
+    /**
+     *
+     */
+    protected function cleanViewsDirectory(): void
+    {
+        $files = (new Finder())->in(resource_path('views'))->files();
+
+        foreach ($files as $file) {
+            unlink($file->getPathname());
+        }
     }
 }
