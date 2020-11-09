34940726be15187675aa58c29aa00207cbb4752f
Amir <a.rami@adaptech.me>
2020-11-09 13:45:28 +0100

    TODO: Tests to write

--- tests/LocalizatorTest.php
+++ tests/LocalizatorTest.php
@@ -4,4 +4,28 @@ namespace Amirami\Localizator\Tests;
 
 class LocalizatorTest extends TestCase
 {
+    public function testLocalizeCommandBasic()
+    {
+        //
+    }
+
+    public function testLocalizeCommandWithDefinedLanguages()
+    {
+        //
+    }
+
+    public function testLocalizeCommandWithSortingJsonKeys()
+    {
+        //
+    }
+
+    public function testLocalizeCommandWithCustomDefinedPatterns()
+    {
+        //
+    }
+
+    public function testLocalizeCommandByMergingTheExistingTranslations()
+    {
+        //
+    }
 }
