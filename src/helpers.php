16132f718e9e541e418ec9811049d94c2d148c3f
Iván Yamil Romero <31389316+knewlife@users.noreply.github.com>
2023-01-14 15:18:55 -0300

    Get correct lang path in Laravel 9.X - Check if is its posible to use the recomended method (https://laravel.com/docs/9.x/upgrade#the-lang-directory) else use legacy resource_path method.

--- src/helpers.php
+++ src/helpers.php
@@ -1,12 +1,20 @@
 <?php
 
-if (! function_exists('lang_path')) {
+if (!function_exists('lang_path')) {
     /**
      * @param string $path
      * @return string
      */
     function lang_path($path = '')
     {
-        return resource_path('lang'.($path !== '' ? DIRECTORY_SEPARATOR.$path : ''));
+        if (
+            function_exists('app') &&
+            method_exists(app(), 'langPath') &&
+            is_dir(app()->langPath())
+        ) {
+            return app()->langPath($path);
+        } else {
+            return resource_path('lang' . ($path !== '' ? DIRECTORY_SEPARATOR . $path : ''));
+        }
     }
 }
